e30c27d5622eabe11043a9232716d8a8c02ec2b3
eocampo21 <114039611+eocampo21@users.noreply.github.com>
2023-10-24 03:51:19 +0000

    feat/analytics: wrong prop name updated

--- analytics/analytics-provider/google-analytics-provider.js
+++ analytics/analytics-provider/google-analytics-provider.js
@@ -2,7 +2,7 @@ export const googleAnalyticsProvider = (eventName, eventProperties) => {
   // Log event using Google Analytics
   if (typeof window.gtag === 'function') {
     window.gtag('event', eventName, eventProperties);
-    console.log(`Event ${eventName} logged with properties:`, updatedEventProperties);
+    console.log(`Event ${eventName} logged with properties:`, eventProperties);
   } else {
     console.warn(`window.gtag is not defined. Event ${eventName} was not logged.`);
   }
